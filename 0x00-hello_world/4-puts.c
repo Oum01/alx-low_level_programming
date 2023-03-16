5bacec5b0f7caf5b8033cf87a75e5bb526a2a575
Oum01 <slimani-oumaima@outlook.com>
2023-03-16 12:28:29 -0700

    why

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main() {
-   /* puts function style  */
-   int puts("Programming is like building a multilingual puzzle ");
-   return 0;
+/**
+* main - Entry point
+*
+* Return: Always 0 (gain)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
