191de4e7cd2b19377a3962e3cc3b8beb6ba98bb7
Oum01 <slimani-oumaima@outlook.com>
2023-03-17 14:57:26 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - NUMBERS FROM 0 TO 9
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58 ; i++)
+	{
+		putchar(i);
+}
+	putchar ('\n');
+	return (0);
+}
