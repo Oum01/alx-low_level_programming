52d1914c7cec770dd6747610aa4a24a0973d27fa
Oum01 <slimani-oumaima@outlook.com>
2023-03-17 14:10:52 -0700

    abc ABC

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - ABC
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int y;
+
+	for (i = 97; i < 123 ; i++)
+	{
+		putchar(i);
+}
+	for (y = 65; y < 91 ; y++)
+	{
+		putchar(y);
+}
+	putchar ('\n');
+	return (0);
+}
