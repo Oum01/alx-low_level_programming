0b7549a5cd61751406dd7b6defa738939879f87e
Oum01 <slimani-oumaima@outlook.com>
2023-03-17 16:00:15 -0700

    12..abc

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - ABC
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+	int y;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		putchar(i);
+	}
+	for (y = 97 ; y <= 102 ; y++)
+	{
+		putchar(y);
+	}
+	putchar ('\n');
+}
