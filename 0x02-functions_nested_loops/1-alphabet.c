a9c69c95eddac234dbdb9879b607d914a0ab937b
Oum01 <slimani-oumaima@outlook.com>
2023-03-21 10:57:21 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints the alphabet in lowercase.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar (alphabet);
+		alphabet++;
+	}
+	_putchar ('\n');
+}
